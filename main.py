22da489f7a09640314579d251fca5deae191c32a
EZCode45 <ezhinduja@gmail.com>
2026-01-29 19:56:09 -0500

    Add import for train_test_split from sklearn

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import polars as pl
+from sklearn.model_selection import train_test_split
 
 splits = {'train': 'train.csv', 'validation': 'val.csv', 'test': 'test.csv'}
 df = pl.read_csv('hf://datasets/lytang/MeetingBank-transcript/' + splits['train'])
