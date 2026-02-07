47bdca8633eb53d6e60e704926b1f1cb35c4ff01
EZCode45 <ezhinduja@gmail.com>
2026-02-07 13:07:04 -0500

    Refactor model training code: import TensorFlow, compile model, and fit on training data

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import polars as pl
 from sklearn.model_selection import train_test_split
-import tensorflow
+import tensorflow as tf
 
 splits = {'train': 'train.csv', 'validation': 'val.csv', 'test': 'test.csv'}
 df = pl.read_csv('hf://datasets/lytang/MeetingBank-transcript/' + splits['train'])
 
 train_df, val_df = train_test_split(df.to_pandas(), test_size=0.2, random_state=42)
-
-
+model = tf.keras.Model()
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(train_df, validation_data=val_df, epochs=5)
+model.evaluate(val_df)
